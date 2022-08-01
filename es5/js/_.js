789beba19c214d02712866a47010678211a39eda
yogoloper <jguddnjs@gmail.com>
2022-08-01 22:36:40 +0900

    _go, _pipe 작성 및 라이브러리 이동

--- es5/js/_.js
+++ es5/js/_.js
@@ -1,15 +1,23 @@
 // _curry
 function _curry(fn) {
   return function (a, b) {
-    return arguments.length == 2 ? fn(a, b) : function (b) { return fn(a, b); }
-  }
+    return arguments.length == 2
+      ? fn(a, b)
+      : function (b) {
+          return fn(a, b);
+        };
+  };
 }
 
 // _curryr
 function _curryr(fn) {
   return function (a, b) {
-    return arguments.length == 2 ? fn(a, b) : function (b) { return fn(b, a); }
-  }
+    return arguments.length == 2
+      ? fn(a, b)
+      : function (b) {
+          return fn(b, a);
+        };
+  };
 }
 
 // _get
@@ -20,7 +28,7 @@ var _get = _curryr(function (obj, key) {
 // _filter
 function _filter(list, predi) {
   var new_ilst = [];
-  _each(list, function(val) { 
+  _each(list, function (val) {
     if (predi(val)) {
       // predi 함수가 실행되면서 평가된 결과가 참/거짓을 반환하고
       new_ilst.push(val); // 참일경우 새로운 리스트에 추가된다.
@@ -32,7 +40,7 @@ function _filter(list, predi) {
 // _map
 function _map(list, mapper) {
   var new_list = [];
-  _each(list, function(val) {
+  _each(list, function (val) {
     new_list.push(mapper(val));
   });
   return new_list;
@@ -45,3 +53,46 @@ function _each(list, iter) {
   }
   return list;
 }
+
+var slice = Array.prototype.slice;
+
+// _rest
+function _rest(list, num) {
+  return slice.call(list, num || 1);
+}
+
+// _reduce
+function _redude(list, iter, memo) {
+  if (arguments.length == 2) {
+    memo = list[0];
+    list = _rest(list);
+  }
+
+  _each(list, function (val) {
+    memo = iter(memo, val);
+  });
+  return memo;
+}
+
+// _pipe
+function _pipe() {
+  var fns = arguments; // 여러 개의 함수를 인자로 받음
+  return function (arg) {
+    return _redude(
+      fns,
+      function (arg, fn) {
+        return fn(arg);
+      },
+      arg
+    );
+  };
+}
+
+// _go
+function _go(arg) {
+  var fns = _rest(arguments); // 첫 요소를 제거 하고 배열로 반환
+  return _pipe.apply(null, fns)(arg);
+}
+
+var _map = _curryr(_map),
+  _filter = _curryr(_filter);
